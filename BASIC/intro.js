e1bf3c17e5fd38974f40e7cc795a0e4f77bbf03b
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2022-05-29 16:29:24 +0530

    Into.css
    -

---
+++ BASIC/intro.js
@@ -0,0 +1,27 @@
+
+        
+            var x;
+            x = "Nishchit"
+
+            
+        
+           // document.getElementById("greet").style.color = "red"
+
+function frnd() {
+            
+                        const frnds = [" Vaibhav, " , " Krishh"] ;
+                        
+           // document.getElementById("frnds1").innerHTML = x ;
+          //  document.getElementById("frnds2").innerHTML = x + " " ;
+            document.getElementById("frnds3").innerHTML = x +" "+ frnds;
+
+        }
+
+function hid() {
+
+            document.getElementById("frnds1").style.display = "none";
+            document.getElementById("frnds2").style.display = "none";
+            document.getElementById("frnds3").style.display = "none";
+        
+}
+        
